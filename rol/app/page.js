2e71fb2af3da8a50f68c2e736ed047f9e6f2d591
Amanda dos Santos Daros <amandsan08@gmail.com>
2025-09-07 23:00:29 -0300

    5

--- rol/app/page.js
+++ rol/app/page.js
@@ -106,7 +106,7 @@ export default function Home() {
           onChange={(e) => setOpcao(e.target.value)}
           placeholder="Digite uma opção"
         />
-        <button onClick={adicionarOpcao} className={styles.add}>Adicionar</button>
+        <button className={styles.add} onClick={adicionarOpcao} >Adicionar</button>
       </div>
 
       <button className={styles.girar} onClick={girarRoleta}>
